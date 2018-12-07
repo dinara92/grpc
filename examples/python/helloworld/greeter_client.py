53d82af98c2ac243c45ac28c57062273d03cd9dd
Dinara Aliyeva <dinara.ali@gmail.com>
2018-12-07 19:08:08 +0900

    helloworld example

--- examples/python/helloworld/greeter_client.py
+++ examples/python/helloworld/greeter_client.py
@@ -21,15 +21,22 @@ import helloworld_pb2
 import helloworld_pb2_grpc
 
 
-def run():
+'''def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
     # used in circumstances in which the with statement does not fit the needs
     # of the code.
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = helloworld_pb2_grpc.GreeterStub(channel)
         response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
-    print("Greeter client received: " + response.message)
+    print("Greeter client received: " + response.message)'''
 
+def run():
+  channel = grpc.insecure_channel('localhost:50051')
+  stub = helloworld_pb2_grpc.GreeterStub(channel)
+  response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
+  print("Greeter client received: " + response.message)
+  response = stub.SayHelloAgain(helloworld_pb2.HelloRequest(name='you'))
+  print("Greeter client received: " + response.message)
 
 if __name__ == '__main__':
     run()
